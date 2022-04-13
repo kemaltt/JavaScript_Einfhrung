a598eefaae93819eca274b0fa7ac493253ae2bc9
kemaltt <ttnckemal@gmail.com>
2022-04-13 16:00:40 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,243 @@
+// Intro---------------------------
+// Lev1_1
+// console.log('Hi');
+// Lev1_2
+// This is a single line JavaScript comment
+// document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+
+// document.write("I have multi-line comments!");
+/*document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");*/
+
+// Lev1_7
+//Coding Conventions
+
+// let helloWorld = 'Hello World'
+// console.log(helloWorld);
+
+// const pi = 3.14
+// console.log(pi);
+
+// let burjDubai = 828 + 'm';
+// console.log(burjDubai);
+
+// let fullName = 'Jan Schmidt';
+// console.log(fullName);
+
+// let eifelTower = 324 + 'm';
+// console.log(eifelTower);
+
+// let camelCase = 'camelCase';
+// console.log(camelCase);
+
+// let kursStatus = true
+// console.log(kursStatus);
+
+// Console.log-------------------------------------------------
+
+//Output Console.log
+
+// a = 12;
+// b = a * 3
+
+// console.log(b); // output 36
+
+// document.write(b)
+
+// console.log(document.URL);
+
+//Variable Deklarieren
+
+// console.log('Hello World');
+// console.log('Anton');
+
+// let name = 'Anton';
+// console.log(name);
+
+// let age = 28;
+// console.log(age);
+
+// let jop = 'Trainer';
+// console.log(jop);
+
+// let married = true
+
+// console.log(married);
+
+// console.log(name + ' ist ' + age + ' Jahre alt, von Beruf ' + jop + ' und ist verheiratet : ' + married);
+
+// Variablen-----------------------
+// Lev1_5
+//Variablen
+
+// let carName = 'BMW'
+// console.log(carName);
+
+// let x = 150
+// console.log(x);
+
+// let y = 50
+// console.log(y);
+
+// let z = x + y
+// console.log(z);
+
+// // ------------------------------------------
+
+// let firstName;
+// let lastName;
+// let age;
+
+// firstName = "John"
+// lastName = "Doe"
+// age = 35
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(age);
+
+// Lev1_6
+//Variablen let const var
+
+// var z = 34;
+// z = 67;
+// console.log(z);
+
+// let x = 34;
+// x = 67;
+// console.log(x);
+
+// const y = 34;
+// y = 67;
+// console.log(y); //error
+
+// Arithmetic-------------------------------------------------
+// Lev1_12
+//Arithmetische Operatoren
+
+// let score = 5 + 5 * 10
+// console.log('Ergebnis :', score);//55
+
+// let score = (5 + 5) * 10;
+// console.log('Ergebnis :', score); //100
+
+// let score = 0;
+// score = score + 10;
+// console.log('Ergebnis: ' + score); //10
+
+// let score = 0;
+// score += 10;
+// console.log('Ergebnis: ' + score);
+
+//Inkrementierung und Dekrementierungen können wie folgt verwendet werden
+
+// let zahl = 1;
+// zahl = zahl + 1;
+// console.log(zahl);
+
+//Inkrement-Operatoren
+
+// zahl++
+// console.log('increment: ' + zahl);
+
+//Dekrement-Operatoren
+// zahl--;
+// console.log('dekrement: ' + zahl)
+
+// Lev1_13
+
+//arithmetische Wiederholung
+
+// Addition
+// let addition_operator = 1 + 1;
+// console.log("addition: " + addition_operator);
+
+// // Subtraktion
+// let subtraktion_operator = 2 - 1;
+// console.log("subtraktion: " + subtraktion_operator);
+
+// // Multiplikation
+// let multiplication_operator = 2 * 2;
+// console.log("multiplication: " + multiplication_operator);
+
+// // Division
+// let division_operator = 4 / 2;
+// console.log("division: " + division_operator);
+
+
+
+// // Modul: zeigt an, was der Rest sein würde.
+// let modulus_operator = 14 % 5;
+// console.log("modulus: " + modulus_operator);
+
+// document.write()-----------------------------------------
+
+//document.write()
+
+
+// document.write('<h1>Hello World</h1>')
+// document.write('<p>Have a nice day </p>')
+
+// let myText = 'Hello again'
+// document.write(myText)
+
+// let vorName = 'Name : Sena'
+// let nachName = 'Nachname : Tütüncü'
+
+// document.write(`<p>${vorName}</p>`)
+// document.write(`<p>${nachName}</p>`)
+
+
+// document.write(nachName)
+
+//String Methods----------------------------------------
+
+// let x = 'Hello'
+// let y = 'World <br>'
+
+// z = x + y
+
+// document.write(z)
+
+// n = x + ' ' + y
+
+// document.write(n)
+
+// x = x + ' World'
+
+// document.write(x)
+
+// let meinString = 'Ich bin Erster'
+// meinString = meinString + ' und  komme auf Platz zwei'
+// console.log(meinString);
+
+
+// window_object
+
+// window.alert("Hallo Welt");
+
+// window.prompt("Bitte geben Sie Ihren Namen ein.");
+
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+
+// window.confirm("Stimmen Sie meiner Meinung zu?");
+
+
+
+// let alter = prompt("Gib mir bitte dein Alter:");
+// console.log(alter);
+
+// let b = 5;
+// let a = b * 5 - 3;
+// alert(a);
+
+// let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+// let grosse = "2,72m";
+// alert(mann + grosse);
